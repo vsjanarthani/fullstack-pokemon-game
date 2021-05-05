b1318781275cca957421c5ef478cb401f1deabf0
Megs <Megs.close@gmail.com>
2021-05-05 15:55:03 -0700

    cards diplaying-needs serious styling help

---
+++ controllers/home-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const fetch = require('node-fetch');
+
+// setting empty array to hold random pokemon ids to pull from api
+let pokeNums = [];
+
+// adding 20 random numbers to our array, making sure there are no repeats
+for (let i = 0; i < 20; i++) {
+    const singlePokeNum = Math.floor(Math.random() * 898) + 1;
+    if (!pokeNums.includes(singlePokeNum)) {
+        pokeNums.push(singlePokeNum);
+    }
+};
+let pokeData = [];
+// looping through our array, using numbers as pokemon to get pokemon data
+const getPokemon = () => {
+
+    for (let i = 0; i < pokeNums.length; i++) {
+        const pokeNum = pokeNums[i];
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
+            .then(response => response.json())
+            .then(data => {
+                let eachPoke = 
+                    {
+                        name: data.name,
+                        hp: data.stats[0].base_stat,
+                        attack: data.stats[1].base_stat,
+                        defense: data.stats[2].base_stat,
+                        speed: data.stats[5].base_stat,
+                        imageSrc: data.sprites.front_default
+                    };
+                pokeData.push(eachPoke);
+            })
+    }
+};
+
+
+getPokemon();
+
+router.get('/draftpage', (req, res) => {
+
+    res.render("draftpage", { pokeData })
+
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const apiRoutes = require("./api");
+const homeRoutes = require('./home-routes');
 
 // const htmlRoutes = require('./Html');
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-// router.use('/', htmlRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
