2f0c45e75309d3d42f6f47ee6fb6880acdb52546
Megs <Megs.close@gmail.com>
2021-05-05 14:34:15 -0700

    getting the pokemon into array to pass to handlebars-question mark

--- public/javascript/draftpage.js
+++ public/javascript/draftpage.js
@@ -1,3 +1,4 @@
+
 // setting empty array to hold random pokemon ids to pull from api
 let pokeNums = [];
 
@@ -8,30 +9,29 @@ for (let i = 0; i < 20; i++) {
         pokeNums.push(singlePokeNum);
     }
 };
-
+let pokeData = [];
 // looping through our array, using numbers as pokemon to get pokemon data
 const getPokemon = () => {
-    
+
     for (let i = 0; i < pokeNums.length; i++) {
         const pokeNum = pokeNums[i];
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
-        .then(response => response.json())
-        // .then(data => console.log(data));
-        .then(data=> {
-            // console.log(data);
-            let pokeName = data.name;
-            console.log(pokeName);
-            let pokeHP = data.stats[0].base_stat;
-            console.log(`HP value is ${pokeHP}`);
-            let pokeAttack = data.stats[1].base_stat;
-            console.log(`Attack value is ${pokeAttack}`);
-            let pokeDefense = data.stats[2].base_stat;
-            console.log(`Defense value is ${pokeDefense}`);
-            let pokeSpeed = data.stats[5].base_stat;
-            console.log(`Speed value is ${pokeSpeed}`);
-        })
+            .then(response => response.json())
+            .then(data => {
+                let eachPoke = 
+                    {
+                        Name: data.name,
+                        HP: data.stats[0].base_stat,
+                        Attack: data.stats[1].base_stat,
+                        Defense: data.stats[2].base_stat,
+                        Speed: data.stats[5].base_stat,
+                        ImageSrc: data.sprites.front_default
+                    };
+                pokeData.push(eachPoke);
+            })
     }
 };
+console.log(pokeData);
 
 getPokemon();
 
