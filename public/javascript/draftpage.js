e0489402902a24762d6242eee890293bf8f1a3da
Megs <Megs.close@gmail.com>
2021-05-08 11:50:59 -0700

    saving stuff

--- public/javascript/draftpage.js
+++ public/javascript/draftpage.js
@@ -4,12 +4,26 @@ let nameEl2 = document.querySelector(".card-title");
 let pokeTeam = []
 
 buttonEl.addEventListener("click", function (event) {
-    let pokemonName = event.target.id;
+    let thisPokemonData = event.target.id;
+    let pokeArray = thisPokemonData.split(" ");
+
+    let pokemonData = {
+        name: pokeArray[0],
+        hp: pokeArray[1],
+        attact: pokeArray[2],
+        defense: pokeArray[3],
+        speed: pokeArray[4],
+        imageSrc: pokeArray[5]
+    }
+    let pokemonName = pokeArray[0];
     console.log(pokemonName);
-    if (pokeTeam.length < 6 && !pokeTeam.includes(pokemonName)) {
-        pokeTeam.push(pokemonName);
+    console.log(pokemonData);
+
+    if (pokeTeam.length < 6 && !pokeTeam.includes(pokemonData)) {
+        pokeTeam.push(pokemonData);
         console.log(pokeTeam);
         let thisButton = document.getElementById(`${pokemonName}`);
+        console.log(`${pokemonName}`)
         thisButton.disabled = true;
         thisButton.innerText = "Already Drafted!"
     }
