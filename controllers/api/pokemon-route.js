4cb0701f348cc41106a3cbb24fdc024e0ba66a2e
Janarthani V Subramaniam <vs.janarthani@gmail.com>
2021-05-11 22:14:53 -0700

    changing production server to env variable

--- controllers/api/pokemon-route.js
+++ controllers/api/pokemon-route.js
@@ -6,7 +6,7 @@ const { sequelize } = require('../../models/User');
 const fetch = require('node-fetch');
 const dev = (process.env.NODE_ENV != 'production');
 
-const server = dev ? 'http://localhost:5000' : 'https://pokemon-draft-fullstack.herokuapp.com';
+const server = dev ? 'http://localhost:5000' : process.env.SERVER_PROD;
 
 // GET /api/pokemons
 router.get('/', async (req, res) => {
