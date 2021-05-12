ba9efaf05743e1678abc8b21c4b35e21d1202e2d
Janarthani V Subramaniam <vs.janarthani@gmail.com>
2021-05-12 05:25:24 -0700

    installing socket and creating a connection

--- controllers/html/battle-route.js
+++ controllers/html/battle-route.js
@@ -3,12 +3,32 @@ const { Team, Pokemon } = require('../../models');
 const sessionAuth = require('../../utils/auth');
 
 
+
+// GET /team of the user
 router.get('/', sessionAuth, (req, res) => {
-   
-    res.render('battle-page');
+    console.log(req.session.user_id);
+    Team.findOne({
+        where: { user_id: req.session.user_id },
+        include: [
+            {
+                model: Pokemon,
+                attributes: ['pokedex', 'pokemon_name', 'pokemon_pic', 'hp', 'attack', 'defense', 'speed'],
+            }]
+    })
+        .then(teamData => {
+            if (teamData) {
+                const userTeam = teamData.get({ plain: true });
+                console.log(userTeam);
+                res.render('battle-page', { userTeam, loggedIn: true, });
+            } else {
+               document.location.replace('/team');
+            }
+        })
+        .catch(e => {
+            console.log(e)
+            res.status(400).json({ Error: e });
+        });
 });
-
-
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
@@ -8,6 +10,8 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const socketServer = http.createServer(app);
+const io = socketio(socketServer);
 const PORT = process.env.PORT || 5000;
 
 const sess = {
@@ -28,10 +32,24 @@ app.use(routes);
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
+// Middleware for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when client connects
+io.on('connection', socket => {
+  console.log('New web socket connection..');
+
+  // msg for the user
+  socket.emit('message', 'Good Job! You are now connected to socket.io');
+  // Broadcast when a user connects (visible to all but the user)
+  socket.broadcast.emit('message', `A user has joined the room`);
+  // msg on disconnect
+  socket.on('disconnect', () => {
+    io.emit('message', `A User has left the room`);
+  });
+});
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false}).then(() => {
-  app.listen(PORT, () => console.log(`Now listening to ${PORT}`));
+  socketServer.listen(PORT, () => console.log(`Now listening to ${PORT}`));
 });
