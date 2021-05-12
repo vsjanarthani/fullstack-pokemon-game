8d8a18e40c8bd334083f3a5421d7d5c63a52bf86
Janarthani V Subramaniam <vs.janarthani@gmail.com>
2021-05-11 22:08:07 -0700

    heroku

--- controllers/api/pokemon-route.js
+++ controllers/api/pokemon-route.js
@@ -4,9 +4,9 @@ const sessionAuth = require('../../utils/auth');
 const { QueryTypes } = require('sequelize');
 const { sequelize } = require('../../models/User');
 const fetch = require('node-fetch');
-const dev = process.env.NODE_ENV !== 'production';
+const dev = (process.env.NODE_ENV != 'production');
 
-const server = dev ? 'http://localhost:5000' : 'https://your_deployment.server.com';
+const server = dev ? 'http://localhost:5000' : 'https://pokemon-draft-fullstack.herokuapp.com';
 
 // GET /api/pokemons
 router.get('/', async (req, res) => {
